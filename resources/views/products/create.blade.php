ba1ca236543e2b0558a10a3c8b039b580b9ccb8f
rafsanrjkhan <rjkhan104@gmail.com>
2023-08-07 22:12:19 +0600

    old text store

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -32,14 +32,14 @@
                         @csrf
                         <div class="form-group">
                            <label>Name</label>
-                           <input type="text" name="name" class="form-control" />
+                           <input type="text" name="name" class="form-control" value="{{ old('name') }}"/>
                            @if ($errors->has('name'))
                                <span class="text-danger">{{ $errors->first('name') }}</span>
                            @endif
                         </div>
                         <div class="form-group">
                             <label>Description</label>
-                            <textarea class="form-control" rows="4" name="description"></textarea>
+                            <textarea class="form-control" rows="4" name="description">{{ old('description') }}</textarea>
                             @if ($errors->has('description'))
                                <span class="text-danger">{{ $errors->first('description') }}</span>
                            @endif
