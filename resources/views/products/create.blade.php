530c37db932c0ceeb598fe7d53e6801b7b6df3a2
rafsanrjkhan <rjkhan104@gmail.com>
2023-08-07 21:56:20 +0600

    errors handle

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -33,14 +33,23 @@
                         <div class="form-group">
                            <label>Name</label>
                            <input type="text" name="name" class="form-control" />
+                           @if ($errors->has('name'))
+                               <span class="text-danger">{{ $errors->first('name') }}</span>
+                           @endif
                         </div>
                         <div class="form-group">
                             <label>Description</label>
                             <textarea class="form-control" rows="4" name="description"></textarea>
+                            @if ($errors->has('description'))
+                               <span class="text-danger">{{ $errors->first('description') }}</span>
+                           @endif
                         </div>
                         <div class="form-group">
                             <label>Image</label>
                             <input type="file" name="image" class="form-control" />
+                            @if ($errors->has('image'))
+                               <span class="text-danger">{{ $errors->first('image') }}</span>
+                           @endif
                         </div>
                         <button type="submit" class="btn btn-dark">Submit</button>
                     </form>
