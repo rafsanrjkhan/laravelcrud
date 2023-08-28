7c2d6c9039318d6c1ac232ad1de042c4b52e9d4f
rafsanrjkhan <rjkhan104@gmail.com>
2023-08-28 23:39:09 +0600

    layouts app

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,33 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Crud</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-  
-    <!-- A grey horizontal navbar that becomes vertical on small screens -->
-    <nav class="navbar navbar-expand-sm bg-dark">
+@extends('layouts.app')
+@section('main')
 
-        <!-- Links -->
-        <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link test-light" href="/">Products</a>
-        </li>
-        </ul>
-    </nav>
-    {{-- flash session --}}
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success alert-block">
-            <strong>{{ $message }}</strong>
-        </div>
-    @endif
     {{-- Container  --}}
     <div class="container">
         <div class="row justify-content-center">
@@ -63,5 +36,4 @@
         </div>
     </div>
     
-</body>
-</html>
+@endsection
